218769d588a0b1f775986e5dbd7cd83663d30de6
TApaquette <59933272+TApaquette@users.noreply.github.com>
2020-01-26 16:28:34 -0500

    added magnet object, got it to move left+right

---
+++ public/js/move-mag-up-component.js
@@ -0,0 +1,38 @@
+AFRAME.registerComponent( 'move-mag-up-component', {
+    init : function() {
+        console.log('right');
+
+        const Context_AF = this;
+        Context_AF.originalScale = Context_AF.el.object3D.scale.clone();
+
+        Context_AF.el.addEventListener('click', function(event) {
+            console.log('click up');
+            Context_AF.moveRight();
+        });
+
+        Context_AF.el.addEventListener('mouseenter', function(event) {
+            //el = html entity or element
+            //object3D = three.js (rendering engine) 3D element
+            Context_AF.el.object3D.scale.set(Context_AF.originalScale.x * 1.1, Context_AF.originalScale.y * 1.1, Context_AF.originalScale.z * 1.1);
+        });
+
+        Context_AF.el.addEventListener('mouseleave', function(event) {
+            Context_AF.el.object3D.scale.set(Context_AF.originalScale.x, Context_AF.originalScale.y, Context_AF.originalScale.z);
+        });
+    },
+    moveRight : function(){
+
+        const Context_AF = this;
+
+        let location = document.getElementById("magnet");
+        let magPos = location.getAttribute("position")
+        const moveIncrement = -0.4
+
+        location.setAttribute('animation', 'property: position; to:'  + (magPos.z + moveIncrement) + '0 7; loop:false; dur: 200')
+        console.log(magPos)
+
+        //add to scene
+        let scene = document.querySelector('a-scene');
+    }
+
+});
